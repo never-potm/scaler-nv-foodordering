90bfb32a4615e8779b3738f2d4f556d30429811e
Suraj Gusain <suraj.gusain15@gmail.com>
2025-01-29 13:23:09 +0530

    initial commit

---
+++ src/main/java/com/scalernv/foodbooking/ScalerNvFoodBookingApplication.java
@@ -0,0 +1,13 @@
+package com.scalernv.foodbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerNvFoodBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerNvFoodBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scalernv/foodbooking/ScalerNvFoodBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalernv.foodbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerNvFoodBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
